5446ee07ab94c57ec2bc8c6e3398741bf458db85
Enrica Garcia <garcia.enricahabito@gmail.com>
2023-03-22 17:22:36 +0100

    add style error page 404

--- src/components/Pages/Error404.js
+++ src/components/Pages/Error404.js
@@ -1,9 +1,21 @@
 import '../../styles/components/Pages/Error404.scss'
+import error from '../../images/error-message.png'
+import { Link } from 'react-router-dom'
 
 const Error404 = () => {
     
     return (
-        <h1 className='title-error-404'>La página no se ha encontrado</h1>
+        <section className='section-error-404'>
+            <img className='section-error-404-image' src={error} alt="error-harry-potter" />
+            <h1 className='section-error-404-title'>
+                ¡Vaya... Esta página se ha ido a buscar otros personajes que no existen!
+            </h1>
+            <p className='section-error-404-text'>
+                Error 404: Lo sentimos, no hemos podido encontrar la página que buscas
+            </p>
+            <p className='section-error-404-paragraph'>Volver a la página inicial:</p>
+            <Link className='section-error-404-link' to='/'>Inicio</Link>
+        </section>
     )
 }
 
